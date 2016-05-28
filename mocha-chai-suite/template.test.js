b4fd48336c99b282e43382ffa3bb7ea437013ae4
Gloria Ma <gloria.ma@aya.yale.edu>
2016-05-28 00:12:41 -0700

    Add mocha-chai-suite with package.json and template test file

---
+++ mocha-chai-suite/template.test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('chai', function() {
+  describe('warmup', function() {
+    it('should be able to do a simple assert', function() {
+      assert(1 === 1);
+    });
+  });
+});
